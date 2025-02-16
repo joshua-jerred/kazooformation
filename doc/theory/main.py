f078e8265493e4f702f1c131906c71db4a35da5d
Joshua Jerred <19292194+joshua-jerred@users.noreply.github.com>
2025-02-16 03:40:07 -0700

    more theory work

--- doc/theory/main.py
+++ doc/theory/main.py
@@ -1,27 +1,65 @@
 import wav_trimmer
 import fft_ploter
 
+SAMPLE_RATE = 44100
 BASE_DIR = "./doc/theory/"
+OUTPUT_DIR = "./doc/theory/output/"
 
 FILES_TO_CONVERT = ["kazooA.mp3", "kazooD.mp3", "kazooE.mp3", "kazooG.mp3"]
-SAMPLE_RATE = 44100
+
+# Where
 START_OFFSET_SECONDS = 1
-SAMPLE_LENGTH_SECONDS = 0.1
-SAMPLE_START_POS = int(SAMPLE_RATE * START_OFFSET_SECONDS)
-SAMPLE_LENGTH = int(SAMPLE_RATE * SAMPLE_LENGTH_SECONDS)  # * seconds
+START_OFFSET_SAMPLES = 100
+SAMPLE_START_POS = int(SAMPLE_RATE * START_OFFSET_SECONDS) + START_OFFSET_SAMPLES
+# What
+# SAMPLE_LENGTH_SECONDS = 0.01
+# SAMPLE_LENGTH = int(SAMPLE_RATE * SAMPLE_LENGTH_SECONDS)
+SAMPLE_LENGTH = 1000
+print(f"Baud rate: {SAMPLE_RATE / SAMPLE_LENGTH}")
+
 for file in FILES_TO_CONVERT:
     wav_trimmer.trim_wav(BASE_DIR, file, SAMPLE_START_POS, SAMPLE_LENGTH, SAMPLE_RATE)
 
+NOISE_FILES = [
+    ("kazooA_sample.wav", 0.2),
+    # ("kazooA_sample.wav", 0.4),
+    # ("kazooA_sample.wav", 0.6),
+    # ("kazooA_sample.wav", 0.8),
+]
+for file, noise_level in NOISE_FILES:
+    wav_trimmer.add_noise(BASE_DIR, OUTPUT_DIR, file, noise_level)
 
-# FFT_INPUT_FILES = []
-FFT_INPUT_FILES = ["kazooA_sample.wav", "kazooD_sample.wav", "kazooE_sample.wav", "kazooG_sample.wav"]
+FFT_INPUT_FILES = []
+FFT_INPUT_FILES = [
+    "kazooE_sample.wav",
+    "kazooA_sample.wav",
+    # f"output/kazooA_sample_noise_0.2.wav",
+    # f"output/kazooA_sample_noise_0.4.wav",
+    # f"output/kazooA_sample_noise_0.6.wav",
+    # f"output/kazooA_sample_noise_0.8.wav",
+    "kazooG_sample.wav",
+    "kazooD_sample.wav",
+    "white_noise.wav",
+]
 # FFT_INPUT_FILES = ["KazooDoHigh_0.1s.wav", "KazooDo_0.1s.wav"]
 fft_params = {
-    "FFT_MIN_FREQ": 1000,
-    "FFT_MAX_FREQ": 10000,
-    "CARRIER_FREQ": 2500,
-    "CARRIER_FREQ_WIDTH": 500,  # 500 Hz on either side of the carrier frequency
+    "FFT_MIN_FREQ": 0,
+    "FFT_MAX_FREQ": 5000,
+    "CARRIER_FREQ": 2000,
+    "CARRIER_FREQ_WIDTH": 100,  # 500 Hz on either side of the carrier frequency
     "FFT_DISTRIBUTION_BINS": 100,
-    # "FFT_DISTRIBUTION_MIN": 
+    "FFT_DISTRIBUTION_MAX": 50,
+    "FFT_DISTRIBUTION_MAX_NORMALIZED": 0.5,
+    "NOTCH_FREQS": [1100, 3080],
+    "NOTCH_HARMONICS": 5,
+    "NOTCH_QUALITY_FACTOR": 30,
+    # "BANDPASS_FREQS": [2500],
+    "BANDPASS_FREQS": [],
+    "BANDPASS_BANDWIDTH": 200,
+    "BANDPASS_ORDER": 5,
+    "NORMALIZE_FFT_FACTOR": 30,
+    "NOISE_FLOOR_VALUE": 0.3,
+    "FUNDAMENTAL_FREQ_MARGIN_HZ": 100,
+    "MAX_FUNDEMENTAL_FREQS": 4,
 }
-fft_ploter.plot_files(BASE_DIR, FFT_INPUT_FILES, fft_params)
+fft_ploter.plot_files(BASE_DIR, OUTPUT_DIR, FFT_INPUT_FILES, fft_params)
--- doc/theory/wav_trimmer.py
+++ doc/theory/wav_trimmer.py
@@ -1,6 +1,7 @@
 from scipy.io import wavfile
 from pydub import AudioSegment
-
+from pydub.generators import WhiteNoise
+from pydub.utils import ratio_to_db
 import numpy as np
 import os
 
@@ -36,3 +37,29 @@ def trim_wav(base_dir, input_file, start_sample_index, trim_length, sample_rate)
     wavfile.write(output_file, sample_rate, trimmed_data)
 
     print(f"Trimmed file saved as {output_file}")
+
+def add_noise(base_dir, output_dir, input_file, noise_level):
+    assert noise_level <= 1.0 and noise_level >= 0.0
+
+    input_file_path = os.path.join(base_dir, input_file)
+    base, ext = os.path.splitext(input_file)
+    output_file = f"{base}_noise_{noise_level}{ext}"
+    output_path = output_dir + output_file
+
+    # source https://stackoverflow.com/a/78926556/20310850
+    audio = AudioSegment.from_file(input_file_path, format="wav")
+    noise = WhiteNoise().to_audio_segment(duration=len(audio))
+    adjusted_noise = noise.apply_gain(ratio_to_db(noise_level))
+    audio = audio.overlay(adjusted_noise)
+    
+    # noise.export(output_dir + "white_noise.wav", format="wav")
+    # sample_rate, data = wavfile.read(file_path)
+
+    # if data.dtype != np.int16:
+        # raise RuntimeError("not 16-bit PCM")
+
+    audio.export(output_path, format="wav")
+
+    # wavfile.write(output_path, sample_rate, data)
+
+    print(f"Added noise to {output_path} - {noise_level}")
